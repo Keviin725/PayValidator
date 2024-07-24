9f77961178d2ef8b04ca1f985b10fb95333553b2
Kevin Machava <72518473+Keviin725@users.noreply.github.com>
2024-07-24 11:14:53 +0200

    save

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -1,15 +1,62 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-form @submit.prevent="processarImagem">
+      <q-uploader v-model="arquivos" label="Upload Talão de Depósito" @uploaded="processarImagem" />
+      <q-btn type="submit" label="Validar Pagamento" />
+      <q-banner v-if="resultado" type="positive">{{ resultado }}</q-banner>
+      <q-banner v-if="erro" type="negative">{{ erro }}</q-banner>
+    </q-form>
   </q-page>
 </template>
 
 <script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+
 defineOptions({
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  setup() {
+    const arquivos = ref([]);
+    const resultado = ref('');
+    const erro = ref('');
+
+    const processarImagem = async () => {
+      const formData = new FormData();
+      formData.append('file', arquivos.value[0]);
+
+      try {
+        const response = await axios.post('http://localhost:5000/extrair_dados', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        const dadosTalao = response.data;
+        validarPagamento(dadosTalao);
+      } catch (error) {
+        erro.value = 'Erro ao processar a imagem.';
+      }
+    };
+
+    const validarPagamento = async (dadosTalao) => {
+      try {
+        const response = await axios.post('http://localhost:5000/validar_pagamento', dadosTalao);
+        resultado.value = response.data.message;
+        erro.value = '';
+      } catch (error) {
+        erro.value = error.response.data.message;
+        resultado.value = '';
+      }
+    };
+
+    return {
+      arquivos,
+      resultado,
+      erro,
+      processarImagem,
+      validarPagamento
+    };
+  }
 });
 </script>
